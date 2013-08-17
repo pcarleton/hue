0b68b32bc670e69e7e2362ad10445953bf956e66
Paul Carleton <paulcarletonjr@gmail.com>
2013-08-16 18:46:11 -0700

    created struct for status message

--- hue.go
+++ hue.go
@@ -8,8 +8,78 @@ import  (
   "io"
 )
 
+type MsgWrapper struct {
+  msg StatusMessage
+}
+
+type StatusMessage struct {
+  Config map[string] interface{}
+  Lights map[string] LightMessage
+  Groups map[string] interface{}
+  Schedules map[string] interface{}
+  Scenes  map[string] interface{}
+}
+
+type LightState struct {
+  On bool
+  Bri int
+  Hue int
+  Sat int
+  Xy []float64
+  Alert string
+  Effect string
+  Colormode string
+  Reachable bool
+}
+
+type LightMessage struct {
+  State LightState
+  Name string
+}
+
+type LightsDict struct {
+  dict map[string] interface{}
+}
+
+type Lights interface{}
+type Groups interface{}
+type Config interface{}
+type Schedules interface{}
+
 const meethue = "http://www.meethue.com/api/nupnp"
 const developer_name = "newdeveloper"
+
+func DoPut(url, data string) {
+  req, err := http.NewRequest("PUT", url, data)
+  if err != nil {
+    fmt.Printf("Error: %v\n", err)
+  }
+  client := http.Client{}
+  resp, err := client.Do(req)
+  fmt.Printf("Response: %v\n", resp)
+
+}
+
+func GetStatusMsg() string {
+  resp, err := http.Get("http://10.0.1.4/api/newdeveloper")
+  if err != nil {
+    fmt.Println("Error: %v", err)
+  }
+  fmt.Println("%v", resp)
+  dec := json.NewDecoder(resp.Body)
+  var m StatusMessage
+  err = dec.Decode(&m)
+  if err != nil {
+    fmt.Println("Error: %v", err)
+  }
+  fmt.Printf("%v\n", m.Lights["1"])
+  resp, err = DoPut("http://10.0.1.4/api/lights/1/state", "{ \"on\":false }")
+  if err != nil {
+    fmt.Println("Error: %v", err)
+  }
+  fmt.Println(resp)
+  return "success"
+}
 
 func ConvertJsonToMap(b io.Reader) (m map[string] interface{}, ok bool) {
   dec := json.NewDecoder(b)
